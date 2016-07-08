1d008a3086e2869098935685cf0461bab180c722
logicmyth <staley77@protonmail.com>
2016-07-08 17:40:40 -0400

    added playlist to song model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ActiveRecord::Base
   # has_one :game, inverse_of: :playlist
-  belongs_to :song, inverse_of: :playlist
-  belongs_to :user, inverse_of: :playlist
+  belongs_to :song, inverse_of: :playlists
+  belongs_to :user, inverse_of: :playlists
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
   belongs_to :answer, inverse_of: :song
   has_many :users, through: :playlists
+  has_many :playlists
 end
