6bdba2fd3ca7ba589d568ef6cbc990b06ede6caf
logicmyth <staley77@protonmail.com>
2016-07-07 21:38:27 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-  Song.create([{ title: 'Big Rock Candy Mountain', artist: 'Harry McClintock', links: 'http://hypem.com/track/16hkh/Harry+McClintock+-+Big+Rock+Candy+Mountain'}, {
-  title: 'Silver Screen', artist: 'Felix Da Housecat', links: 'http://hypem.com/track/1jt60/Felix+Da+Housecat+-+Silver+Screen+(Shower+Scene)'}])
+  Song.create([{title: 'I Want a House', artist: 'Mr. Twin Sister', links: 'http://hypem.com/track/18war/Twin+Sister+-+I+Want+a+House'},
+  { title: 'Marble House', artist: 'The Knife', links: 'http://hypem.com/track/5kwy/The+Knife+-+Marble+House'},
+  { title: 'Big Rock Candy Mountain', artist: 'Harry McClintock', links: 'http://hypem.com/track/16hkh/Harry+McClintock+-+Big+Rock+Candy+Mountain'},
+  {title: 'Silver Screen', artist: 'Felix Da Housecat', links: 'http://hypem.com/track/1jt60/Felix+Da+Housecat+-+Silver+Screen+(Shower+Scene)'}])
