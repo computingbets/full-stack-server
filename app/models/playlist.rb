34a9c4693c8892187bcdc58d9c9a22eae6c04003
logicmyth <staley77@protonmail.com>
2016-07-08 17:29:08 -0400

    playing with playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ActiveRecord::Base
-  has_one :game, inverse_of: :playlist
-  belongs_to :song
-  belongs_to :user
+  # has_one :game, inverse_of: :playlist
+  belongs_to :song, inverse_of: :playlist
+  belongs_to :user, inverse_of: :playlist
 end
